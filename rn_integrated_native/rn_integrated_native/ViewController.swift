99df165a79de15e2303542ba8abc4f3369305a04
Tetsuro Mikami <t.mikami1992@gmail.com>
2018-06-03 20:45:50 +0900

    Create iOS project with Xcode and put it in the root directory of RN project

---
+++ rn_integrated_native/rn_integrated_native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rn_integrated_native
+//
+//  Created by Tetsuro Mikami on 2018/06/03.
+//  Copyright © 2018 Tetsuro Mikami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
